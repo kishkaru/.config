e9037c8a22d8934b4817d3581465ff18b63c5c13
Kishan Karunaratne <kishkaru@gmail.com>
2013-06-22 23:18:49 -0700

    Removed unnecessary files files

--- dot_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-HOME = os.getenv("HOME")
-
-confs = {
-        'tmux': [('f', '.tmux.conf', HOME + '/.tmux.conf'), ('d', '.tmux', HOME + '/.tmux'), ],
-        'vim': [('f', '.vimrc', HOME + '/.vimrc'), ('d', '.vim', HOME + '/.vim'), ],
-        'git': [('f', '.gitconfig', HOME + '/.gitconfig'), ('f', '.gitignore_global', HOME + '/.gitignore_global')],
-        }
